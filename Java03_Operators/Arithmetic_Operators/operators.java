7edb602b0953711a6f6bfabe21ecfc1cb27bf885
Ruzait Ahamed <111993066+ruzait@users.noreply.github.com>
2024-11-13 02:20:26 +0000

    operators

---
+++ Java03_Operators/Arithmetic_Operators/operators.java
@@ -0,0 +1,34 @@
+public class operators {
+
+    public static void main(String[] args) {
+
+        /*  Binary operators    */
+        int a = 4;
+        int b = 2;
+
+        int c = a + b;      // Addition
+        int d = a - b;      // Subtraction
+        int e = a * b;      // Multiplication
+        int f = a / b;      // Division
+
+        int g = 3 % 2;      // Modulus
+
+
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+
+        float z = (float)a/(float)b;
+        System.out.println(z);
+
+        /* Unary operators */
+
+        // Increment
+
+
+
+        // Decrement
+    }
+}
