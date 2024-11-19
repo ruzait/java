b66165cbd5b6fd2646ced05e06f35ac1c7bbeaec
Ruzait Ahamed <111993066+ruzait@users.noreply.github.com>
2024-11-19 02:18:06 +0000

    loops

---
+++ Java05_ItaratedStatment/_04_JumpStatment/jumpCon.java
@@ -0,0 +1,13 @@
+public class jumpCon {
+    
+    public static void main(String[] args) {
+        
+        for (int i=1; i <= 5; i++){
+            if (i==3){
+                //break;
+                continue;
+            }
+            System.out.println(i);
+        }
+    }
+}
